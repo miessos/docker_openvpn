ede890f18abd84070a41557a7e0f1fefdb173486
miessos <miessos@woohoo-two>
2016-02-06 15:37:29 +0100

    .

--- createtun.sh
+++ createtun.sh
@@ -2,5 +2,5 @@
 touch /root/test2.txt
 [ -d /root/net ] ||
     mkdir -p /root/net
-[ -c /dev/net/tun ] ||
-    mknod /dev/net/tun c 10 200
+#[ -c /dev/net/tun ] ||
+#    mknod /dev/net/tun c 10 200
